d4b07ccdaf4e2ee721dd931f1c0a98e2cceb26ab
aleksa-radojicic <aleksa.radojicic@outlook.com>
2022-12-18 17:39:37 +0100

    Fixed mistake in migrations

--- database/migrations/2022_12_18_132003_removed_image5_from_products_table.php
+++ database/migrations/2022_12_18_132003_removed_image5_from_products_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('product', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->dropColumn('image5');
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('product', function (Blueprint $table) {
+        Schema::table('products', function (Blueprint $table) {
             $table->string('image5');
         });
     }
