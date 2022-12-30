023f00012f6ec9e41b09541a8392c6e7ca06583c
aleksa-radojicic <aleksa.radojicic@outlook.com>
2022-12-30 16:45:19 +0100

    Added AuthController and added API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderItemController;
+use App\Http\Controllers\OrderOrderItemController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserOrderController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +23,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::resource('orders', OrderController::class)->only(['store']);
+
+    Route::resource('order_items', OrderItemController::class)->only(['store']);
+
+    Route::resource('users.orders', UserOrderController::class)->only(['index']);
+
+    Route::resource('orders.order_items', OrderOrderItemController::class)->only(['index']);
+
+    Route::resource('products', ProductController::class)->only(['index', 'show']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+Route::resource('products', ProductController::class)->only(['index', 'show']);
+
+Route::get('/login', function () {
+    return response()->json('Please log in');
+})->name('login');
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
